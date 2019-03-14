5d1967a6279dd33e30ebb4f4e3f43ed6d3f105c8
Matías Lang <matiasl@infobytesec.com>
2019-03-14 18:34:34 -0300

    Add constants module

--- sample/__init__.py
+++ sample/__init__.py
@@ -1,4 +1,6 @@
+from __future__ import print_function
+from sample.constants import CONST
 
 def main():
     """Entry point for the application script"""
-    print("Call your main application code here")
+    print("Call your main application code here", CONST)
